624e86835089b779f4b8fa2062fd67561cc65d52
Ariel Galvez <Ariel.Galvez@fundacion-jala.org>
2019-03-06 21:23:28 -0400

    The tests are missing, tomorrow complete

---
+++ Monitoring/Monitoring/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Monitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("enter number: ");
+                var input = Console.ReadLine();
+                var output = Parser.BuildRomanNumber(int.Parse(input));
+                Console.WriteLine($"number {input} in roman is {output.Symbol}");
+            }
+        }
+    }
+}
---
+++ Monitoring/Monitoring/RomanNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monitoring
+{
+    public class RomanNumber
+    {
+        public int Value { get; private set; }
+        public string Symbol { get; private set; }
+        public RomanNumber(int value, string symbol)
+        {
+            Value = value;
+            Symbol = symbol;
+        }
+    }
+}
